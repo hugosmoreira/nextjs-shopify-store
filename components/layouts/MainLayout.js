dad61057e5d2c963c44f2a778e72f3552627d286
hugosmoreira <hugo.n4@gmail.com>
2022-02-01 17:05:55 -0800

    Headers

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -1,9 +1,10 @@
+import Header1 from '../Header/Header1/Header1';
 import React from 'react';
 
 export default function mainLayout(props) {
   return (
      <>
-        <header>This is the Header</header>
+        <Header1 />
         <main>{props.children}</main>
         <header>This is the footer</header>
      </>
--- pages/_document.js
+++ pages/_document.js
@@ -8,9 +8,9 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html>``
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta  content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
