6959f73847ba161f1d012e051dfaff91f60bc6ca
hugosmoreira <hugo.n4@gmail.com>
2022-02-01 23:37:21 -0800

    hero1 and hero 2 with scss

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -1,12 +1,14 @@
-import Header1 from '../Header/Header1/Header1';
-import React from 'react';
+import Header1 from '../Header/Header1/Header1'
+import Header2 from '../Header/Header2/Header2'
+import Header3 from '../Header/Header3/Header3'
+import Hero1 from '../Hero/Hero1/Hero1'
+import Hero2 from '../Hero/Hero2/Hero2'
 
-export default function mainLayout(props) {
-  return (
-     <>
-        <Header1 />
-        <main>{props.children}</main>
-        <header>This is the footer</header>
-     </>
-  )
+export default function MainLayout(props){
+  return(<div className="fixed-nav transparent-nav">
+    <Header3 />
+    <Hero2 />
+    <main >{props.children}</main>
+    <header>This is the Footer</header>
+  </div>)
 }
--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>``
+      <Html>
         <Head>
           <meta  content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
