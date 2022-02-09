4154f83f4b61c1e8e1d9aa20354ece5f38b12372
hugosmoreira <hugo.n4@gmail.com>
2022-02-08 16:30:40 -0800

    collections

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -3,11 +3,13 @@ import Header2 from '../Header/Header2/Header2'
 import Header3 from '../Header/Header3/Header3'
 import Hero1 from '../Hero/Hero1/Hero1'
 import Hero2 from '../Hero/Hero2/Hero2'
+import Collection1 from '../Collections/Collection1/Collection1'
 
 export default function MainLayout(props){
   return(<div className="fixed-nav transparent-nav">
     <Header3 />
     <Hero2 />
+      <Collection1 />
     <main >{props.children}</main>
     <header>This is the Footer</header>
   </div>)
